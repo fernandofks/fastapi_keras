6f5e89a66322db44340ec89fc5bbad7fdec06a6c
Fernando Sakabe <fernandokenjisakabe@gmail.com>
2022-07-27 14:36:07 -0300

    com get

--- main.py
+++ main.py
@@ -17,22 +17,26 @@ input = np.array([[[0.08, 0.14, 0.09, 0.18],
        [0.1 , 0.32, 0.07, 0.07],
        [0.1 , 0.29, 0.07, 0.07],
        [0.09, 0.24, 0.07, 0.07]]])
-class UserInput(BaseModel):
-    user_input: str
-@app.get('/')
-async def index():
-    return {"Message": "This is Index"}
-
-@app.post('/predict/') 
-async def predict(UserInput: UserInput):
+# class UserInput(BaseModel):
+#     user_input: str
+@app.get('/{UserInput}')
+async def predict(UserInput):
     global input
-    lista_string=UserInput.user_input
+    lista_string=UserInput
     lista= ast.literal_eval(lista_string)
     input = np.insert(input[0], 0,lista).reshape(1,11,4)
     input = input[:,:-1,:]
     prediction = MODEL.predict(input)
-    
-
     return {"prediction": float(prediction)}
+
+# @app.post('/predict/') 
+# async def predict(UserInput: UserInput):
+#     global input
+#     lista_string=UserInput.user_input
+#     lista= ast.literal_eval(lista_string)
+#     input = np.insert(input[0], 0,lista).reshape(1,11,4)
+#     input = input[:,:-1,:]
+#     prediction = MODEL.predict(input)
+#     return {"prediction": float(prediction)}
 
 
