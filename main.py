d2c15bfa5cc450b82b52467547a204af4587e743
Fernando Kenji Sakabe <fernandoks@al.insper.edu.br>
2022-11-08 12:11:40 -0300

    fast api com 9 linhas

--- main.py
+++ main.py
@@ -8,16 +8,15 @@ import time
 MODEL = tf.keras.models.load_model('model.h5')
 
 app = FastAPI()
-input = np.array([[[-0.23,  0.59,  0.02,  0.16,  0.66,  0.09],
-       [-0.23,  0.59,  0.01,  0.15,  0.66,  0.02],
-       [-0.23,  0.59,  0.01,  0.13,  0.66, -0.04],
-       [-0.23,  0.59,  0.02,  0.13,  0.66, -0.06],
-       [-0.22,  0.6 ,  0.11,  0.13,  0.66, -0.06],
-       [-0.24,  0.6 ,  0.31,  0.13,  0.66, -0.06],
-       [-0.24,  0.62,  0.38,  0.13,  0.66, -0.05],
-       [-0.26,  0.6 ,  0.32,  0.13,  0.66, -0.05],
-       [-0.27,  0.59,  0.22,  0.13,  0.66, -0.05],
-       [-0.25,  0.6 ,  0.14,  0.13,  0.66, -0.05]]])
+input = np.array([[[-0.198,  0.629,  0.259,  0.094,  0.628,  0.307],
+       [-0.198,  0.629,  0.259,  0.094,  0.628,  0.307],
+       [-0.198,  0.629,  0.259,  0.094,  0.628,  0.307],
+       [-0.198,  0.629,  0.259,  0.094,  0.628,  0.307],
+    [-0.198,  0.629,  0.259,  0.094,  0.628,  0.307],
+        [-0.198,  0.629,  0.259,  0.094,  0.628,  0.307],
+       [-0.198,  0.628,  0.259,  0.094,  0.628,  0.307],
+       [-0.198,  0.628,  0.259,  0.094,  0.628,  0.307],
+       [-0.198,  0.628,  0.259,  0.094,  0.628,  0.307]]])
 # class UserInput(BaseModel):
 #     user_input: str
 @app.get('/{UserInput}')
@@ -26,10 +25,9 @@ async def predicting(UserInput):
     global input
     lista_string=UserInput
     lista= ast.literal_eval(lista_string)
-    input = np.append(input[0],[lista],axis=0).reshape(1,11,6)
+    input = np.append(input[0],[lista],axis=0).reshape(1,10,6)
     input = input[:,1:,:]
     input_diff=np.diff(input, axis=1)
-    print(input_diff)
     prediction = MODEL.predict(input_diff)
     vel = np.argmax(prediction)
     process_time = time.time() - start_time
